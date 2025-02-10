7b3c1fba52980db488cee094596edc9b47eb979f
iamnas <iamnas.eth@gmail.com>
2025-02-10 13:20:16 +0530

    soleved vecs2

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,9 +4,15 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+        output.push(element * 2);
     }
 
     output
+}
+
+
+fn times_two(element: &i32) -> i32 {
+    *element * 2
 }
 
 fn vec_map_example(input: &[i32]) -> Vec<i32> {
@@ -21,12 +27,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     // by 2, but with iterator mapping instead of manually pushing into an empty
     // vector.
     // See the example in the function `vec_map_example` above.
-    input
-        .iter()
-        .map(|element| {
-            // ???
-        })
-        .collect()
+    input.iter().map(times_two).collect()
 }
 
 fn main() {
