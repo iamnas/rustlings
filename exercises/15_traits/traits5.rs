50df41fd9053ec1575c1723cee5b23305e57b0fd
iamnas <iamnas.eth@gmail.com>
2025-02-12 01:52:30 +0530

    solved 14 15 16

--- exercises/15_traits/traits5.rs
+++ exercises/15_traits/traits5.rs
@@ -19,7 +19,7 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // TODO: Fix the compiler error by only changing the signature of this function.
-fn some_func(item: ???) -> bool {
+fn some_func<S:SomeTrait + OtherTrait>(item: S) -> bool {
     item.some_function() && item.other_function()
 }
 
