94e93260f29d938ac187788898e2e3a6d80ebde7
iamnas <iamnas.eth@gmail.com>
2025-02-12 19:09:49 +0530

    iterators

--- exercises/18_iterators/iterators4.rs
+++ exercises/18_iterators/iterators4.rs
@@ -10,6 +10,16 @@ fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
+
+    // let mut result = [1; num];
+
+    // (0..num).fold(1, |acc, x| acc * x)
+
+    // (1..=num).fold(1, |acc, x| acc * x)
+    (1..=num).product()
+
+    
+    
 }
 
 fn main() {
